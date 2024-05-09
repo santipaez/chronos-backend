b0aa8b914e259676a7529f667137e8484a85a89a
santipaez <sr.paez@alumno.um.edu.ar>
2024-05-09 17:13:45 -0300

    Estructurado del proyecto y spring security

---
+++ src/main/java/com/eventz/model/User.java
@@ -0,0 +1,58 @@
+package com.eventz.model; // Update the package declaration to match the expected package "com.eventz.model"
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+  private String username;
+  private String email;
+  private String password;
+  private Integer age;
+
+  // Getters and Setters
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+}
---
+++ src/main/java/com/eventz/restservice/Greeting.java
@@ -0,0 +1,3 @@
+package com.eventz.restservice;
+
+public record Greeting(long id, String content) { }
